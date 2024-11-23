681afda9659b240f00d88706edf34f9eea2618a3
Brandon Dykun <brandon.dykun@gmail.com>
2024-11-23 12:09:57 -0500

    feat: add row count prop to post tile skeleton
    Added prop for number of rows. It will now render the number of rows passed, defaults to 6.

--- components/LoadingSkeletons/PostTileSkeleton.tsx
+++ components/LoadingSkeletons/PostTileSkeleton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { IContentLoaderProps } from "react-content-loader";
 import ContentLoader, { Rect } from "react-content-loader/native";
 import { Dimensions } from "react-native";
@@ -7,51 +7,54 @@ import { COLORS } from "@/constants/Colors";
 import { useColorMode } from "@/context/ColorModeContext";
 
 type Props = {
+  rows?: number;
   props?: IContentLoaderProps;
 };
 
 // svg skeleton to mimic post tiles during loading state
 
-const PostTileSkeleton = ({ ...props }: Props) => {
+const PostTileSkeleton = ({ rows = 6, ...props }: Props) => {
   const screenWidth = Dimensions.get("window").width;
   const { isDarkMode } = useColorMode();
 
   // size of each post tile
   const size = screenWidth / 3 - 2; // subtract 2 to account for gap
 
-  const firstPosition = 0; // first position offset
-  const secondPosition = size + 2; // second position offset
-  const thirdPosition = size * 2 + 2 * 2; // third position offset
-  const fourthPosition = size * 3 + 2 * 3; // fourth position offset
+  const firstPosition = 2; // first position offset
+  const secondPosition = size + 4; // second position offset
+  const thirdPosition = size * 2 + 4; // third position offset
 
   // svg area height
-  const viewHeight = fourthPosition + size;
+  const viewHeight = size * rows + 2 * rows;
 
   const bgColor = isDarkMode ? COLORS.zinc[900] : COLORS.zinc[200]; // tile color
   const highlightColor = isDarkMode ? COLORS.zinc[800] : COLORS.zinc[300]; // swiping tile highlight color
 
+  // create correctly positioned row for each row in rows prop
+  const tiles = [...Array(rows)].map((_, i) => {
+    // offset down from top for the entire row
+    const y = i === 0 ? 0 : i * size + 2 * i;
+
+    return (
+      <Fragment key={i}>
+        <Rect x={firstPosition} y={y} rx="0" ry="0" width={size} height={size} />
+        <Rect x={secondPosition} y={y} rx="0" ry="0" width={size - 2} height={size} />
+        <Rect x={thirdPosition} y={y} rx="0" ry="0" width={size} height={size} />
+      </Fragment>
+    );
+  });
+
   return (
     <ContentLoader
-      speed={2}
+      speed={1}
       width={screenWidth}
-      height={fourthPosition + size}
+      height={viewHeight}
       viewBox={`0 0 ${screenWidth} ${viewHeight}`}
       backgroundColor={bgColor}
       foregroundColor={highlightColor}
       {...props}
     >
-      <Rect x={firstPosition} y={firstPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={secondPosition} y={firstPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={thirdPosition} y={firstPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={firstPosition} y={secondPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={secondPosition} y={secondPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={thirdPosition} y={secondPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={firstPosition} y={thirdPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={secondPosition} y={thirdPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={thirdPosition} y={thirdPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={firstPosition} y={fourthPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={secondPosition} y={fourthPosition} rx="0" ry="0" width={size} height={size} />
-      <Rect x={thirdPosition} y={fourthPosition} rx="0" ry="0" width={size} height={size} />
+      {tiles}
     </ContentLoader>
   );
 };
