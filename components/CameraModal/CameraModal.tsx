a0f779e7b675aa9f0dc9753eb11ba344b250a6cd
Brandon Dykun <brandon.dykun@gmail.com>
2025-02-10 14:16:26 -0500

    fix: correct image crop aspect in image picker
    Made aspect 1 x 1 in image picker crop. Only applicable to android.

--- components/CameraModal/CameraModal.tsx
+++ components/CameraModal/CameraModal.tsx
@@ -69,7 +69,7 @@ const CameraModal = ({ visible, setVisible, images, setImages, maxImages, onSave
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     });
 
